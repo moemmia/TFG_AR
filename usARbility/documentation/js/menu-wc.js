5a8f62bdbb409a128c4e7a286ca790bc8bea8884
Moisés Muñiz Mangas <36801152+moemmia@users.noreply.github.com>
2020-05-30 22:26:53 +0200

    update configuration + google services

--- usARbility/documentation/js/menu-wc.js
+++ usARbility/documentation/js/menu-wc.js
@@ -72,13 +72,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="modules/AppModule.html" data-type="entity-link">AppModule</a>
                                     <li class="chapter inner">
                                         <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
-                                            'data-target="#components-links-module-AppModule-1f7dba76625b73dc68d3910005549508"' : 'data-target="#xs-components-links-module-AppModule-1f7dba76625b73dc68d3910005549508"' }>
+                                            'data-target="#components-links-module-AppModule-9b37c9b459ffa7f00c49bd69b89def6b"' : 'data-target="#xs-components-links-module-AppModule-9b37c9b459ffa7f00c49bd69b89def6b"' }>
                                             <span class="icon ion-md-cog"></span>
                                             <span>Components</span>
                                             <span class="icon ion-ios-arrow-down"></span>
                                         </div>
-                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-AppModule-1f7dba76625b73dc68d3910005549508"' :
-                                            'id="xs-components-links-module-AppModule-1f7dba76625b73dc68d3910005549508"' }>
+                                        <ul class="links collapse" ${ isNormalMode ? 'id="components-links-module-AppModule-9b37c9b459ffa7f00c49bd69b89def6b"' :
+                                            'id="xs-components-links-module-AppModule-9b37c9b459ffa7f00c49bd69b89def6b"' }>
                                             <li class="link">
                                                 <a href="components/AppComponent.html"
                                                     data-type="entity-link" data-context="sub-entity" data-context-id="modules">AppComponent</a>
@@ -87,13 +87,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                     </li>
                                 <li class="chapter inner">
                                     <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
-                                        'data-target="#injectables-links-module-AppModule-1f7dba76625b73dc68d3910005549508"' : 'data-target="#xs-injectables-links-module-AppModule-1f7dba76625b73dc68d3910005549508"' }>
+                                        'data-target="#injectables-links-module-AppModule-9b37c9b459ffa7f00c49bd69b89def6b"' : 'data-target="#xs-injectables-links-module-AppModule-9b37c9b459ffa7f00c49bd69b89def6b"' }>
                                         <span class="icon ion-md-arrow-round-down"></span>
                                         <span>Injectables</span>
                                         <span class="icon ion-ios-arrow-down"></span>
                                     </div>
-                                    <ul class="links collapse" ${ isNormalMode ? 'id="injectables-links-module-AppModule-1f7dba76625b73dc68d3910005549508"' :
-                                        'id="xs-injectables-links-module-AppModule-1f7dba76625b73dc68d3910005549508"' }>
+                                    <ul class="links collapse" ${ isNormalMode ? 'id="injectables-links-module-AppModule-9b37c9b459ffa7f00c49bd69b89def6b"' :
+                                        'id="xs-injectables-links-module-AppModule-9b37c9b459ffa7f00c49bd69b89def6b"' }>
                                         <li class="link">
                                             <a href="injectables/AppFacade.html"
                                                 data-type="entity-link" data-context="sub-entity" data-context-id="modules" }>AppFacade</a>
